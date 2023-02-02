48d3dfe434f7fd3d4d0da753852fc8c031c84afb
Pratik Mahajan <pratiksmahajan15@gmail.com>
2023-02-03 00:18:47 +0530

    Added new expample of delegates

--- Concepts/Delegates.cs
+++ Concepts/Delegates.cs
@@ -11,17 +11,17 @@ namespace GeeksForGeeks
         // be same as the return type and parameter type
         // of the two methods
         // "addnum" and "subnum" are two delegate names
-        public delegate void addnum(int a, int b);
-        public delegate void subnum(int a, int b);
+        public delegate void twoNumCalc(int a, int b);
+        //public delegate void subnum(int a, int b);
 
         // method "sum"
-        public void sum(int a, int b)
+        public static void sum(int a, int b)
         {
             Console.WriteLine("(100 + 40) = {0}", a + b);
         }
 
         // method "subtract"
-        public void subtract(int a, int b)
+        public static void subtract(int a, int b)
         {
             Console.WriteLine("(100 - 60) = {0}", a - b);
         }
@@ -31,18 +31,20 @@ namespace GeeksForGeeks
         {
 
             // creating object "obj" of class "Geeks"
-            Geeks obj = new Geeks();
+            //Geeks obj = new Geeks();
 
             // creating object of delegate, name as "del_obj1"
             // for method "sum" and "del_obj2" for method "subtract" &
             // pass the parameter as the two methods by class object "obj"
             // instantiating the delegates
-            addnum del_obj1 = new addnum(obj.sum);
-            subnum del_obj2 = new subnum(obj.subtract);
+            twoNumCalc del_add = new twoNumCalc(sum);
+            twoNumCalc del_sub = new twoNumCalc(subtract);
 
             // pass the values to the methods by delegate object
-            del_obj1(100, 40);
-            del_obj2(100, 60);
+            del_add(100, 40);
+            del_sub(100, 60);
+			
+			Console.Read();
 
             // These can be written as using
             // "Invoke" method
---
+++ Concepts/Delegates2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GeeksForGeeks
+{
+
+    delegate void MeDelegate();
+
+    class Geeks
+    {
+        void foo()
+        {
+            Console.WriteLine("Fooooooooo");
+        }
+
+        static void goo()
+        {
+            Console.WriteLine("goooooooo");
+        }
+
+        public static void Main(String[] args)
+        {
+            MeDelegate del = new MeDelegate(new Geeks().foo); // can be written as del = new Geeks().foo
+            del();
+            del = new MeDelegate(goo); //can also be written as del = goo;
+            del();
+            Console.Read();
+        }
+    }
+}
