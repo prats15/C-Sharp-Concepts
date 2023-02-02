ed39f3a551b99a4dd3db34e43f4c8cabf67d813c
Pratik Mahajan <pratiksmahajan15@gmail.com>
2023-02-02 12:28:29 +0530

    Added Extension method and Type conversion operator

---
+++ Concepts/ExtensionMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace MySpace
+{
+    class Cow
+    {
+        public int mooCount;
+    }
+
+    static class cowExtensionMethod
+    {
+        public static void Moo(this Cow cow)
+        {
+            cow.mooCount++;
+            Console.WriteLine("Mooo " + cow.mooCount);
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main()
+        {
+            Cow c1 = new Cow();
+            c1.Moo(); c1.Moo(); c1.Moo();
+            Console.Read();
+        }
+    }
+}
---
+++ Concepts/TypeConversionOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace MySpace
+{
+    class Scooter
+    {
+        public int mileage { get; set; }
+        public static implicit operator Car(Scooter scoot)
+        {
+            return new Car { mileage = scoot.mileage };
+        }
+    }
+
+    class Car
+    {
+        public int mileage { get; set; }
+    }
+    class MainClass
+    {
+        public static void Main()
+        {
+            Scooter s = new Scooter();
+            Car c = s;
+        }
+    }
+}
